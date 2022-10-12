04ae1443779dd6655058151f52cb76697a3e3565
Rovnin Nikita <Nikita.Rovnin@gmail.com>
2022-10-12 16:24:38 +0700

    init

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, db_index=True)
+
